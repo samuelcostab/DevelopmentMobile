1e1fd0010f1a267d2f2c275dc751590f3dd9c31c
samuelcostab <samuelufcbrito@gmail.com>
2020-03-06 01:48:37 -0300

    Adicionando Botões e Visualização do Menu

--- CardapioNew/App.js
+++ CardapioNew/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, ImageBackground, FlatList } from 'react-native';
+import { StyleSheet, View, Text, ImageBackground, TouchableOpacity, Button } from 'react-native';
 import MenuDay from './src/components/MenuDay';
+import HomeScreen from './HomeScreen';
+import SettingsScreen from './SettingsScreen';
 
 const menuDays = [
   {
@@ -30,10 +32,25 @@ const menuDays = [
   }
 ];
 
+/* <FlatList
+            data={menuDays}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => {
+              return (
+                <MenuDay menuDay={item} />
+              )
+            }}
+          /> */
+
 export default class App extends Component {
+  state = {
+    indexMenuDays: 1,
+  }
+
   render() {
     return (
-       <View style={styles.container}>
+      <View style={styles.container}>
+
         <View style={styles.boxTitle}>
 
           <ImageBackground style={styles.imgBackground}
@@ -44,16 +61,43 @@ export default class App extends Component {
           </ImageBackground>
         </View>
 
+        <View style={styles.buttonsBox}>
+          <TouchableOpacity style={styles.buttonStyle}
+          onPress={() => {
+              this.setState({ indexMenuDays: 0 })
+            }}>
+            <Text style={styles.subTitleBox}>Segunda-feira</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle}
+          onPress={() => {
+              this.setState({ indexMenuDays: 1 })
+            }}>
+            <Text style={styles.subTitleBox}>Terça-feira</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle}
+          onPress={() => {
+              this.setState({ indexMenuDays: 2 })
+            }}>
+            <Text style={styles.subTitleBox}>Quarta-feira</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle}
+          onPress={() => {
+              this.setState({ indexMenuDays: 3 })
+            }}>
+            <Text style={styles.subTitleBox}>Quinta-feira</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonStyle}
+          onPress={() => {
+              this.setState({ indexMenuDays: 4 })
+            }}>
+            <Text style={styles.subTitleBox}>Sexta-feira</Text>
+          </TouchableOpacity>
+          
+        </View>
+        
         <View style={styles.listMenuDay}>
-          <FlatList
-            data={menuDays}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => {
-              return (
-                <MenuDay menuDay={item} />
-              )
-            }}
-          />
+          <MenuDay menuDay={menuDays[this.state.indexMenuDays]} />
         </View>
       </View>
     );
@@ -73,27 +117,36 @@ const styles = StyleSheet.create({
     borderBottomColor: "#8B0000",
     flex: 1,
   },
-  boxShadow: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#8B0000",
-    opacity: 0.7,
-    borderRadius: 30,
-  },
   titleBox: {
     textAlign: 'center',
     fontSize: 30,
     fontWeight: 'bold',
     color: 'white',
-    marginBottom: 10,
+    marginBottom: 20,
   },
   subTitleBox: {
     textAlign: 'center',
     fontSize: 20,
     fontWeight: 'bold',
-    color: 'black',
+    color: 'white',
     marginBottom: 10,
+  },
+  buttonsBox: {
+    flex: 2,
+    marginTop: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonStyle:{
+    backgroundColor: "#8B0000",
+    width: 300,
+    height:40,
+    justifyContent: 'center',
+    marginBottom: 10,
+    borderRadius: 30,
+    shadowColor: '#000',
+    shadowOpacity: 0.7,
+
   },
   imgBackground: {
     resizeMode: 'cover',
@@ -105,7 +158,8 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   listMenuDay: {
-    flex: 2
+    flex: 1,
+    marginBottom: 25,
   }
 
 
